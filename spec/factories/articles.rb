713dd6991485ccadd7ce55de4bc828babf72564e
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-01-29 10:28:14 -0300

    feat: added test validation to content and slugs

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :article do
-    title { "Sample article" }
-    content { "Sample content" }
-    slug { "sample-article" }
+    sequence(:title) { |n| "Sample Article #{n}" }
+    # sequence(:content) { |n| "Sample content #{n}" }
+    content {"Sample Content"}
+    sequence(:slug) { |n| "Sample Slug-#{n}" }
   end
 end
