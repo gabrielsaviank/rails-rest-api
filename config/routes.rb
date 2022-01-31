399b96a4bc8bccc341b90c4eef46580cef44b20d
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-01-31 11:26:06 -0300

    chore: fixed first test typo and added articles%show test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/articles', to: 'articles#index'
   # routes :articles
-  resources :articles, only: [:index]
+  resources :articles, only: %i[index show]
 end
